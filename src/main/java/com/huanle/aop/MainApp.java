cba129befd8fe249b4e25438f0e69523167606f3
Huanle Hu <huanlehu@Huanles-MacBook-Pro.local>
2018-05-06 17:41:31 +0900

    update comment

--- src/main/java/com/huanle/aop/MainApp.java
+++ src/main/java/com/huanle/aop/MainApp.java
@@ -10,10 +10,10 @@ public class MainApp {
 	    ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
 	    
 //	    使用动态字节码生成技术实现AOP原理是在运行期间目标字节码加载后，生成目标类的子类，
-//	    将切面逻辑加入到子类中，所以cglib实现AOP不需要基于接口,即被代理的类不是接口的实现也没问题
-//	    所以这里能(PrinterService) myServiceProxy行得通
+//	    将切面逻辑加入到子类中，所以cglib实现AOP不需要基于接口,即被代理的类不是接口的实现也没问题，使用继承也能完成
+//	    所以这里能(PrinterService) myServiceProxy行得通，因为cust是子类对象
 	    PrinterService cust = (PrinterService) appContext.getBean("myServiceProxy");
-	    
+
 	    cust.printName();
 	    System.out.println("******************");
 	    cust.printURL();
